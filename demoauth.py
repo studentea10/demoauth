a18162275ef5c4793456cc3c05af88e90e46b856
studentea10 <153243601+studentea10@users.noreply.github.com>
2023-12-22 13:13:47 +0000

    Update demoauth.py

--- demoauth.py
+++ demoauth.py
@@ -17,21 +17,22 @@ def verify_otp(secret_key, user_input_otp):
     # Verify the entered OTP
     return totp.verify(user_input_otp)
 
-# Example Usage:
+
 if __name__ == "__main__":
-    # Step 1: Generate a secret key for the user
+    # Generate a secret key for the user
     secret_key = generate_secret_key()
     print(f"Secret Key: {secret_key}")
 
-    # Step 2: Display the QR code (optional, for mobile authenticator apps)
+    # Display the QR/SecretKey code (optional, for mobile authenticator apps)
     totp = pyotp.TOTP(secret_key)
     print(f"Scan the following QR code in your authenticator app:\n{totp.provisioning_uri('your-username', issuer_name='YourApp')}")
 
-    # Step 3: Simulate user login
+    # ADD user login
     user_entered_otp = input("Enter the OTP from your authenticator app: ")
 
-    # Step 4: Verify the entered OTP
+    #  Verify the entered OTP
     if verify_otp(secret_key, user_entered_otp):
         print("Authentication Successful!")
     else:
         print("Authentication Failed!")
+
